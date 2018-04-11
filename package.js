c107d1bbbda25eebc41037589cb26a7182d0af5b
Tamino Martinius <dev@zaku.eu>
2018-04-11 15:57:33 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,13 +1,17 @@
 Package.describe({
   name: 'zaku:deep-extend',
   summary: 'Recursive object extending.',
-  version: '0.9.0_1',
-  git: 'https://github.com/Zaku-eu/meteor-deep-extend.git'
+  version: '0.9.1',
+  git: 'https://github.com/tamino-martinius/meteor-deep-extend.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
+  api.use([
+    'ecmascript@0.9.0'
+  ], ['client', 'server']);
   api.addFiles('zaku:deep-extend.js');
+  if (api.export) api.export('deepExtend');
 });
 
 Package.onTest(function(api) {
@@ -17,6 +21,5 @@ Package.onTest(function(api) {
     'test-helpers',
     'tinytest'
   ], ['client', 'server']);
-  api.add_files('zaku:deep-extend-tests.coffee',['client', 'server']);
-  if (api.export) api.export("deepExtend");
+  api.add_files('zaku:deep-extend-tests.coffee', ['client', 'server']);
 });
